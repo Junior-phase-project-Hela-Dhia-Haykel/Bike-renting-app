df51953ca7637ef8b21e375e3e76b0b499c20609
HaykelHadjSalem <haykel_hadj@yahoo.fr>
2020-10-15 14:11:47 +0100

    first step structuring the app

---
+++ database/bike.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const db = require('./index.js');
+mongoose.Promise = global.Promise;
+
+const bikeSchema = new mongoose.Schema({
+  model: String,
+  price: Number,
+  imageUrl: String,
+  description: String,
+  daysOfService: {type: Number, default: 0}
+}, 
+  {
+    timestamps: true
+  }
+);
+
+const Bike = db.model('Bike', bikeSchema);
+
+module.exports = Bike;
