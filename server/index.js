b1f3a18fcc2a6b42c30ebe4daff2ff229e0a956f
helayounsi <hela.ahmedyounsi@gmail.com>
2020-10-16 17:03:54 +0100

    add and update user

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '../../dist'));
 
-//add a user to the dataBase:
+//add or update a user to the dataBase:
 //app.post("/userInformation", (req,res)=>{
 //     addNewClient(req.body).then((response)=>{
  //         res.send(response._id);
